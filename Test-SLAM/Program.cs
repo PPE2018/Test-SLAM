797975c0e6e8a926080a2a2b1d31dd8d2f703cb3
jouffroy <jouffroy@UTILISA-KQT89BT>
2018-12-04 17:20:40 +0100

    test

--- Test-SLAM/Program.cs
+++ Test-SLAM/Program.cs
@@ -10,7 +10,7 @@ namespace Test_SLAM
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello world");
+            Console.WriteLine("hello world !!!!");
         }
     }
 }
